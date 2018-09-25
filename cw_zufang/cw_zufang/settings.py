81447a73490c141a8cc938c798bb09027017ce5d
smallren101 <small_pupil@126.com>
2018-09-25 18:05:03 +0800

    master finished

--- cw_zufang/cw_zufang/settings.py
+++ cw_zufang/cw_zufang/settings.py
@@ -1,90 +1,62 @@
 # -*- coding: utf-8 -*-
-
-# Scrapy settings for cw_zufang project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://doc.scrapy.org/en/latest/topics/settings.html
-#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'cw_zufang'
 
 SPIDER_MODULES = ['cw_zufang.spiders']
 NEWSPIDER_MODULE = 'cw_zufang.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cw_zufang (+http://www.yourdomain.com)'
-
+#item Pipeline同时处理item的最大值为100
+# CONCURRENT_ITEMS=100
+#scrapy downloader并发请求最大值为16
+#CONCURRENT_REQUESTS=4
+#对单个网站进行并发请求的最大值为8
+#CONCURRENT_REQUESTS_PER_DOMAIN=2
+#抓取网站的最大允许的抓取深度值
+DEPTH_LIMIT=0
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
-
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
+DOWNLOAD_TIMEOUT=10
+DNSCACHE_ENABLED=True
+#避免爬虫被禁的策略1，禁用cookie
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
+CONCURRENT_REQUESTS=4
+#CONCURRENT_REQUESTS_PER_IP=2
+#CONCURRENT_REQUESTS_PER_DOMAIN=2
+#设置下载延时，防止爬虫被禁
+DOWNLOAD_DELAY = 5
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
+    "cw_zufang.proxy_mw.ProxyMiddleware":100,
+    'scrapy.downloadermiddlewares.robotstxt.RobotsTxtMiddleware': 100,
+    'scrapy.downloadermiddlewares.defaultheaders.DefaultHeadersMiddleware': 550,
+    'scrapy.downloadermiddlewares.ajaxcrawl.AjaxCrawlMiddleware': 560,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 590,
+    'scrapy.downloadermiddlewares.chunked.ChunkedTransferMiddleware': 830,
+    'scrapy.downloadermiddlewares.stats.DownloaderStats': 850,
+    'cw_zufang.timeout_mw.Timeout_Middleware':610,
+    'scrapy.downloadermiddlewares.httpauth.HttpAuthMiddleware': None,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': 300,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    'scrapy.downloadermiddlewares.redirect.MetaRefreshMiddleware': None,
+    'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 400,
+    'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': None,
+    'scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware': None,
+    'cw_zufang.userAgent_mw.RotateUserAgentMiddleware':400,
+    'cw_zufang.redirect_mw.Redirect_Middleware':500,
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+}
+#使用scrapy-redis组件，分布式运行多个爬虫
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
 
-# Enable or disable spider middlewares
-# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'cw_zufang.middlewares.CwZufangSpiderMiddleware': 543,
-#}
+#配置日志存储目录
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+SCHEDULER_PERSIST = True
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderPriorityQueue'
+REDIS_URL = None
+REDIS_HOST = '127.0.0.1' # 也可以根据情况改成 localhost
+REDIS_PORT = '6379'
+#LOG_FILE = "logs/scrapy.log"
 
-# Enable or disable downloader middlewares
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'cw_zufang.middlewares.CwZufangDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'cw_zufang.pipelines.CwZufangPipeline': 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
